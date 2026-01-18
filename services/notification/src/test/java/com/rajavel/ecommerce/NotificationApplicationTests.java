177403c9574664b293161b6361321ce1df48c6eb
Rajavel Murugan <rajavelmurugan123@gmail.com>
2026-01-18 09:36:52 +0530

    The data added

---
+++ services/notification/src/test/java/com/rajavel/ecommerce/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajavel.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
